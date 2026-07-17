9925e3c02168578b7e001650c4dc9249c33726a8
Trh <38323745+aka40x@users.noreply.github.com>
2026-07-17 08:11:32 +0700

    Update students_router.py

--- Session 20/routers/students_router.py
+++ Session 20/routers/students_router.py
@@ -12,7 +12,7 @@ router = APIRouter(
 )
 
 @router.get("/", response_model=list[StudentResponse], status_code=status.HTTP_200_OK)
-def get_all_student(db: Session = Depends(handle_connect_DB)):
+def get_all_data_student(db: Session = Depends(handle_connect_DB)):
     students = students_service.handle_get_all_student(db = db)
     return students
 
